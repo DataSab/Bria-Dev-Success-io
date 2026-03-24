b88b68bf834951a2902cc88dba6568889458a6ce
DataSab <135612979+DataSab@users.noreply.github.com>
2026-03-24 17:39:51 +0100

    Handle vid timer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
     });
 
     const handleTimeUpdate = () => {
-      if (video.currentTime >= 30) {
+      if (video.currentTime >= 26) {
         video.currentTime = 0;
         video.play().catch(() => {});
       }
